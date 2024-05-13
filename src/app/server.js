ba4503131cefc4c981aca05849249b62e2a10599
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2024-05-13 14:47:44 +0530

    Issue #ED-0000: morgan log

--- src/app/server.js
+++ src/app/server.js
@@ -145,7 +145,7 @@ app.all([
   '/content-editor/telemetry', '/discussion/*', '/collection-editor/telemetry', '/v1/user/*', '/sessionExpired', '/logoff', '/logout', '/assets/public/*', '/endSession',
   '/sso/sign-in/*', '/v1/desktop/handleGauth', '/v1/desktop/google/auth/success', '/clearSession', '/kendra/*', '/dhiti/*', '/assessment/*', '/cloudUpload/*', '/apple/auth/*',
   '/uci/*'
-],session({
+],morgan(morganConfig),session({
     secret: envHelper?.PORTAL_SESSION_SECRET_KEY,
     resave: false,
     cookie: {
